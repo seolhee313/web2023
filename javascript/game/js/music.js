007f712de7b23390e330b3cd8a6d9e5dfdf60cce
seolhee313 <125417882+seolhee313@users.noreply.github.com>
2023-04-25 18:25:53 +0900

    2023.04.25

---
+++ javascript/game/js/music.js
@@ -0,0 +1,53 @@
+const allMusic = [
+    {
+        name : "1. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view01.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "2. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view02.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "3. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view03.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "4. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view04.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "5. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view05.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "6. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view06.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "7. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view07.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "8. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view08.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "9. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view09.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    },{
+        name : "10. 저리가라",
+        artist : "TrackTribe",
+        img : "music_view10.png",
+        audio: "Calm Cam - TrackTribe.mp3"
+    }
+]
