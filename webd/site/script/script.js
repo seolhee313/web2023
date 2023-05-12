c22296c52239f776ff81c98c38235a788736bf9c
seolhee313 <125417882+seolhee313@users.noreply.github.com>
2023-05-12 16:44:37 +0900

    2023.05.12

---
+++ webd/site/script/script.js
@@ -0,0 +1,43 @@
+$(function(){
+    $(".nav > ul > li").on({
+        mouseover: function() {
+            $(this).find(".submenu").stop().slideDown(200);
+        },
+        focusin: function() {
+            $(this).find(".submenu").stop().slideDown(200);
+        },
+        mouseout: function() {
+            $(this).find(".submenu").stop().slideUp(200);
+        },
+        focusout: function() {
+            $(this).find(".submenu").stop().slideUp(200);
+        }
+    });
+
+    //슬라이드 : 페이드 효과
+    let currentIndex = 0;
+    const $slider = $(".slider");
+    $slider.hide().first().show();  //모든 이미지 숨기고 첫번째 이미지 나타나게 함
+
+    setInterval(function(){
+        let nextIndex = (currentIndex + 1) % $slider.length;
+
+        $slider.eq(currentIndex).fadeOut(1200);
+        $slider.eq(nextIndex).fadeIn(1200);
+
+        currentIndex = nextIndex;
+
+    }, 3000)
+
+    //탭메뉴
+    const tabBtn = $(".info-menu > a");
+    const tabCont = $(".info-cont > ul");
+    tabCont.hide().eq(0).show();
+
+    tabBtn.on("click", function(){
+        const index = $(this).index();
+
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+});
